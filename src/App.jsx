7b81afe5b8c2b43abe0586e0d9a8c55bd734b986
Nick <nick.esselman@gmail.com>
2026-02-12 12:16:09 +0100

    added article

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { HelloWorld } from './helloworld'
+import { Article } from './article'
 
 function App() {
   return (
     <div className="app">
-      <HelloWorld title="Hello World" />
+      <HelloWorld title="HelloWorld" />
+      <Article 
+        title="Title"
+        text="Text"
+      />
     </div>
   )
 }
