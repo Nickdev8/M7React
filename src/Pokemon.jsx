62babb01d13a7239fb94c094d11a9de06604bb53
Nick <nick.esselman@gmail.com>
2026-02-19 12:08:49 +0100

    added anohtr json

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -11,6 +11,13 @@ export function Pokemon() {
         setPokemon(data);
         console.log(data);
       });
+    fetch("https://pokeapi.co/api/v2/pokemon-species/254")
+      .then((response) => response.json())
+      .then((data) => {
+        newdata = [...pokemon.name, data]
+        setPokemon(newdata);
+        console.log(data);
+      });
   }, []);
 
   if (pokemon === null) {
@@ -20,20 +27,21 @@ export function Pokemon() {
       </section>
     );
   } else {
-  return (
-    <ul className={styles.pokemon}>
-      <li>
-        <p className={styles.name}>{pokemon.name}</p>
-        <img src={pokemon.sprites.front_default} />
-        <p className={styles.id}>{pokemon.id}</p>
+    return (
+      <ul className={styles.pokemon}>
+        <li>
+          <p className={styles.name}>{pokemon.name}</p>
+          <img src={pokemon.sprites.front_default} />
+          <p className={styles.id}>{pokemon.id}</p>
+          <p className={styles.id}>{pokemon.id}</p>
 
-        <ul>
-          {pokemon.types.map((typeObj, index) => (
-            <li key={index}>{typeObj.type.name}</li>
-          ))}
-        </ul>
-      </li>
-    </ul>
-  );
-}
+          <ul>
+            {pokemon.types.map((typeObj, index) => (
+              <li key={index}>{typeObj.type.name}</li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+    );
+  }
 }
